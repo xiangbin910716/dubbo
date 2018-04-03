da42b424dbe56e0ff2166e7146b188f2de05a7d2
jixd <jixd5@ziroom.com>
2018-04-03 17:02:40 +0800

    1.增加动态产生的代码

--- dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
@@ -11,7 +11,7 @@ public class Consumer {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
         context.start();
-
+        System.out.println("启动后");
         DemoService demoService = (DemoService) context.getBean("demoService"); // 获取远程服务代理
         String hello = demoService.sayHello("world"); // 执行远程方法
 
